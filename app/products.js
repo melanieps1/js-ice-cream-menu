167ca24720168f91804e1e91bae420da8734437f
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-29 08:49:31 -0400

    headers and fonts in place

---
+++ app/products.js
@@ -0,0 +1,79 @@
+var products = {
+  
+  "Regular Flavors": [
+
+    {
+      "name": "Chocolate",
+      "description": "Loved by kids of all ages!",
+      "price": "2.00"
+    },
+
+    {
+      "name": "Vanilla",
+      "description": "Elegant and understated",
+      "price": "2.00"
+    },
+
+    {
+      "name": "Chocolate Chip Cookie Dough"
+      "description": "The best ice cream ever!!!"
+      "price": "2.00"
+    }
+
+  ],
+
+  "Seasonal Flavors": [
+
+    {
+      "name": "Pumpkin Pie",
+      "description": "Pumpkin ice cream with a graham cracker swirl",
+      "price": "3.00"
+    },
+
+    {
+      "name": "Apple Pie",
+      "description": "Made with local farm fresh apples",
+      "price": "3.00"
+    }
+
+  ],
+
+  "Serving Options": [
+
+    {
+      "name": "Waffle Cone",
+      "description": "Made fresh by hand in our store each day",
+      "price": "1.00"
+    },
+
+    {
+      "name": "Cup",
+      "description": "Plenty of room for toppings",
+      "price": "0.00"
+    }
+
+  ],
+
+  "Toppings": [
+
+    {
+      "name": "Hot Fudge",
+      "description": "Rich and chocolatey and just sweet enough",
+      "price": "0.50"
+    },
+
+    {
+      "name": "Whipped Cream",
+      "description": "Need we say more?",
+      "price": "0.50"
+    },
+
+    {
+      "name": "Rainbow Sprinkles",
+      "description": "Like a million tiny little smiles",
+      "price": "0.50"
+    }
+
+  ]
+
+};
