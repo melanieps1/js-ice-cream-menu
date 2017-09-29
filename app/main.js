5c730a4ea5ab4fece7f31c27b9cd09d8aa7a5a78
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-29 11:06:43 -0400

    got all array elements in

--- app/main.js
+++ app/main.js
@@ -1,7 +1,41 @@
-var regFlavors = [
-    [ "Chocolate","Loved by kids of all ages!","2.00" ],
-    [ "Vanilla","Elegant and understated","2.00" ],
-    [ "Chocolate Chip Cookie Dough","The best ice cream ever!!!","2.00" ]
-  ];
-
 document.getElementById('regFlavor-1').innerHTML = regFlavors[0][0];
+document.getElementById('regFlavor-2').innerHTML = regFlavors[1][0];
+document.getElementById('regFlavor-3').innerHTML = regFlavors[2][0];
+document.getElementById('desc1').innerHTML = regFlavors[0][1];
+document.getElementById('desc2').innerHTML = regFlavors[1][1];
+document.getElementById('desc3').innerHTML = regFlavors[2][1];
+
+document.getElementById('seasonalFlavor-1').innerHTML = seasonalFlavors[0][0];
+document.getElementById('seasonalFlavor-2').innerHTML = seasonalFlavors[1][0];
+document.getElementById('desc4').innerHTML = seasonalFlavors[0][1];
+document.getElementById('desc5').innerHTML = seasonalFlavors[1][1];
+
+document.getElementById('serve-1').innerHTML = servingOptions[0][0];
+document.getElementById('serve-2').innerHTML = servingOptions[1][0];
+document.getElementById('serve-3').innerHTML = servingOptions[2][0];
+document.getElementById('desc6').innerHTML = servingOptions[0][1];
+document.getElementById('desc7').innerHTML = servingOptions[1][1];
+document.getElementById('desc8').innerHTML = servingOptions[2][1];
+document.getElementById('servePrice1').innerHTML = servingOptions[0][2];
+document.getElementById('servePrice2').innerHTML = servingOptions[1][2];
+document.getElementById('servePrice3').innerHTML = servingOptions[2][2];
+
+document.getElementById('top-1').innerHTML = toppings[0][0];
+document.getElementById('top-2').innerHTML = toppings[1][0];
+document.getElementById('top-3').innerHTML = toppings[2][0];
+document.getElementById('desc9').innerHTML = toppings[0][1];
+document.getElementById('desc10').innerHTML = toppings[1][1];
+document.getElementById('desc11').innerHTML = toppings[2][1];
+document.getElementById('topPrice1').innerHTML = toppings[0][2];
+document.getElementById('topPrice2').innerHTML = toppings[1][2];
+document.getElementById('topPrice3').innerHTML = toppings[2][2];
+
+// Wait for page to load before doing anything
+// document.onreadystatechange = function() {
+// };
+
+// function regIceCream() {
+// 	for (i = 0; i < regFlavors.length; i++) {
+		// Do something
+// 	}
+// }
--- app/products.js
+++ app/products.js
@@ -2,4 +2,21 @@ var regFlavors = [
     [ "Chocolate","Loved by kids of all ages!","2.00" ],
     [ "Vanilla","Elegant and understated","2.00" ],
     [ "Chocolate Chip Cookie Dough","The best ice cream ever!!!","2.00" ]
-  ];
+];
+
+var seasonalFlavors = [
+	[ "Pumpkin Pie","Pumpkin ice cream with a graham cracker swirl","3.00" ],
+	[ "Apple Pie","Made with local farm fresh apples","3.00" ]
+];
+
+var servingOptions = [
+	[ "Cup","Plenty of room for toppings","0.00" ],
+	[ "Sugar Cone","Made fresh by hand in our store each day","0.50" ],
+	[ "Waffle Cone","Delicious and our largest cone","1.00" ]
+];
+
+var toppings = [
+	[ "Hot Fudge","Rich and chocolatey and just sweet enough","1.00" ],
+	[ "Whipped Cream","Need we say more?","1.00" ],
+	[ "Rainbow Sprinkles","Like a million tiny little smiles","0.50" ]
+];
