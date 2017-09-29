4cd2306da11bdd01239f2d60072253044dd0d3de
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-09-29 09:59:09 -0400

    got a js array displaying in html

---
+++ app/main.js
@@ -0,0 +1,7 @@
+var regFlavors = [
+    [ "Chocolate","Loved by kids of all ages!","2.00" ],
+    [ "Vanilla","Elegant and understated","2.00" ],
+    [ "Chocolate Chip Cookie Dough","The best ice cream ever!!!","2.00" ]
+  ];
+
+document.getElementById('regFlavor-1').innerHTML = regFlavors[0][0];
